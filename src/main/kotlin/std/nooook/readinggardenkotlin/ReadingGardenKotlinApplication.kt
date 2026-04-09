21364abf5bfba6acf1a719cec7e0958a123fdc91
Ian Kim <devonlyian@gmail.com>
2026-04-09 17:43:36 +0900

    fix: harden native deploy startup

--- src/main/kotlin/std/nooook/readinggardenkotlin/ReadingGardenKotlinApplication.kt
+++ src/main/kotlin/std/nooook/readinggardenkotlin/ReadingGardenKotlinApplication.kt
@@ -2,11 +2,13 @@ package std.nooook.readinggardenkotlin
 
 import org.springframework.context.annotation.ImportRuntimeHints
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import std.nooook.readinggardenkotlin.common.config.HibernateNativeHints
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableScheduling
 @ImportRuntimeHints(HibernateNativeHints::class)
 class ReadingGardenKotlinApplication
